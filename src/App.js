f216458be289840d84adb231af7c76ab2243421d
shivam <shivamtrivanshi2@gmail.com>
2018-07-03 11:45:27 +0530

    refactored fetch call

--- src/App.js
+++ src/App.js
@@ -13,9 +13,7 @@ class App extends Component {
     }
   }
 
-  componentDidMount() {
-    this.setState({news: []});
-    const newsURL = `https://newsapi.org/v2/everything?sources=the-times-of-india,the-hindu&language=en&pageSize=100&sortBy=publishedAt&apiKey=${apiKey.apiKey}`;
+  callfetch(newsURL) {
     fetch(newsURL)
     .then((res) => {
       return res.json();
@@ -25,159 +23,86 @@ class App extends Component {
     })
     .catch((err) => {
       console.log(err);
-    })
+    });
+  }
+
+  componentDidMount() {
+    this.setState({news: []});
+    const newsURL = `https://newsapi.org/v2/everything?sources=the-times-of-india,the-hindu&language=en&pageSize=100&sortBy=publishedAt&apiKey=${apiKey.apiKey}`;
+
+    this.callfetch(newsURL);
   }
 
   getScienceNews() {
     this.setState({news: []});
     const newsURL = `https://newsapi.org/v2/everything?sources=national-geographic,next-big-future&pageSize=100&sortBy=publishedAt&apiKey=${apiKey.apiKey}`;
-    fetch(newsURL)
-    .then((res) => {
-      return res.json();
-    })
-    .then((news) => {
-      return this.setState({news: news.articles});
-    })
-    .catch((err) => {
-      console.log(err);
-    })
+
+    this.callfetch(newsURL);
   }
 
   getSportNews() {
     this.setState({news: []});
     const newsURL = `https://newsapi.org/v2/everything?sources=bleacher-report,the-times-of-india,the-hindu&q=cricket,football&pageSize=100&sortBy=publishedAt&apiKey=${apiKey.apiKey}`;
-    fetch(newsURL)
-    .then((res) => {
-      return res.json();
-    })
-    .then((news) => {
-      return this.setState({news: news.articles});
-    })
-    .catch((err) => {
-      console.log(err);
-    })
+
+    this.callfetch(newsURL);
   }
 
   getEntertenmentNews() {
     this.setState({news: []});
     const newsURL = `https://newsapi.org/v2/everything?sources=buzzfeed,entertainment-weekly,ign,mtv-news&language=en&pageSize=100&sortBy=publishedAt&apiKey=${apiKey.apiKey}`;
-    fetch(newsURL)
-    .then((res) => {
-      return res.json();
-    })
-    .then((news) => {
-      return this.setState({news: news.articles});
-    })
-    .catch((err) => {
-      console.log(err);
-    })
+
+    this.callfetch(newsURL);
   }
 
   getPoliticsNews() {
     this.setState({news: []});
     const newsURL = `https://newsapi.org/v2/everything?sources=the-times-of-india,the-hindu&q=politic&pageSize=100&sortBy=publishedAt&apiKey=${apiKey.apiKey}`;
-    fetch(newsURL)
-    .then((res) => {
-      return res.json();
-    })
-    .then((news) => {
-      return this.setState({news: news.articles});
-    })
-    .catch((err) => {
-      console.log(err);
-    })
+
+    this.callfetch(newsURL);
   }
 
   getBusinessNews() {
     this.setState({news: []});
     const newsURL = `https://newsapi.org/v2/everything?sources=the-times-of-india,the-hindu&q=business&pageSize=100&sortBy=publishedAt&apiKey=${apiKey.apiKey}`;
-    fetch(newsURL)
-    .then((res) => {
-      return res.json();
-    })
-    .then((news) => {
-      return this.setState({news: news.articles});
-    })
-    .catch((err) => {
-      console.log(err);
-    })
+
+    this.callfetch(newsURL);
   }
 
   getTechnologyNews() {
     this.setState({news: []});
     const newsURL = `https://newsapi.org/v2/everything?sources=wired,the-verge,the-next-web,techcrunch,techradar&language=en&pageSize=100&sortBy=publishedAt&apiKey=${apiKey.apiKey}`;
-    fetch(newsURL)
-    .then((res) => {
-      return res.json();
-    })
-    .then((news) => {
-      return this.setState({news: news.articles});
-    })
-    .catch((err) => {
-      console.log(err);
-    })
+
+    this.callfetch(newsURL);
   }
 
   getMoviesNews() {
     this.setState({news: []});
     const newsURL = `https://newsapi.org/v2/everything?q=movies&language=en&pageSize=100&sortBy=publishedAt&apiKey=${apiKey.apiKey}`;
-    fetch(newsURL)
-    .then((res) => {
-      return res.json();
-    })
-    .then((news) => {
-      return this.setState({news: news.articles});
-    })
-    .catch((err) => {
-      console.log(err);
-    })
+
+    this.callfetch(newsURL);
   }
 
   getCricketNews() {
     this.setState({news: []});
     const newsURL = `https://newsapi.org/v2/everything?sources=espn-cric-info&language=en&pageSize=100&sortBy=publishedAt&apiKey=${apiKey.apiKey}`;
-    fetch(newsURL)
-    .then((res) => {
-      return res.json();
-    })
-    .then((news) => {
-      return this.setState({news: news.articles});
-    })
-    .catch((err) => {
-      console.log(err);
-    })  
+
+    this.callfetch(newsURL);  
   }
 
 
   getBollyWoodNews() {
     this.setState({news: []});
     const newsURL = `https://newsapi.org/v2/everything?q=bollywood&pageSize=100&language=en&sortBy=publishedAt&apiKey=${apiKey.apiKey}`;
-    fetch(newsURL)
-    .then((res) => {
-      return res.json();
-    })
-    .then((news) => {
-      return this.setState({news: news.articles});
-    })
-    .catch((err) => {
-      console.log(err);
-    })  
+
+    this.callfetch(newsURL);
   }
 
 
   getHollywoodNews() {
     this.setState({news: []});
     const newsURL = `https://newsapi.org/v2/everything?q=hollywood&pageSize=100&language=en&sortBy=publishedAt&apiKey=${apiKey.apiKey}`;
-    fetch(newsURL)
-    .then((res) => {
-      return res.json();
-    })
-    .then((news) => {
-      return this.setState({news: news.articles});
-    })
-    .catch((err) => {
-      console.log(err);
-    }) 
+ 
+    this.callfetch(newsURL);
   }
 
 
--- src/News.js
+++ src/News.js
@@ -11,21 +11,21 @@ class News extends Component {
   }
 
 
-  render() {
+  render() {         
     return(
-        <div className="card">
-          <div className="card-image">
-            <img
-            src={this.props.urlToImage === null || "" ? this.state.urlToImage : this.props.urlToImage} alt="ImageNotFound"/>
-          </div>
-          <div className="card-content">
-            <span className="card-title">{this.props.title}</span>
-            <p className="justify">{this.props.description}</p>
-          </div>
-          <div className="card-action">
-            <a href={this.props.url} target="_blank" className="btn">Show More</a>
-          </div>
-        </div>
+      <div className="card">
+      <div className="card-image">
+        <img
+        src={this.props.urlToImage === null || "" ? this.state.urlToImage : this.props.urlToImage} alt="ImageNotFound"/>
+      </div>
+      <div className="card-content">
+        <span className="card-title">{this.props.title}</span>
+        <p className="justify">{this.props.description}</p>
+      </div>
+      <div className="card-action">
+        <a href={this.props.url} target="_blank" className="btn">Show More</a>
+      </div>
+    </div>   
     )
   }
 }
